b440a83da516653bf2159e6e74c2003eb9ff3e4f
Siendo Miguel <siendomiguel@gmail.com>
2020-11-13 22:05:47 -0500

    arreglando tamaño de imagenes y otros detalles

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 document.querySelector('.menu-btn').addEventListener('click', () => {
     document.querySelector('.nav-menu').classList.toggle('show');
 });
+
+//Usamos la funcion de aparecer cuando hacemos scroll
+ScrollReveal().reveal('.cuerpo');
+ScrollReveal().reveal('.news-cards', { delay: 200});
+ScrollReveal().reveal('.cards-banner-one', { delay: 200});
+ScrollReveal().reveal('.cards-banner-two', { delay: 200});
+ScrollReveal().reveal('.social', { delay: 200});
+ScrollReveal().reveal('.footer-links', { delay: 200});
+ScrollReveal().reveal('.footer', { delay: 200});
